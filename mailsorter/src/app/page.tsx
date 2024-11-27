bb172e8225f06a992ca9cbdf966783bcdb9c7c97
Timoté Ballochi <tballochi99@gmail.com>
2024-11-27 20:03:06 +0100

    test

--- mailsorter/src/app/page.tsx
+++ mailsorter/src/app/page.tsx
@@ -1,6 +1,6 @@
-import MailSorter from '../app/components/MailSorter';
-import { Intro } from '../app/components/Intro';
-import { Footer } from '../app/components/Footer';
+import MailSorter from '@/app/components/MailSorter';
+import { Intro } from '@/app/components/Intro';
+import { Footer } from '@/app/components/Footer';
 
 export default function Home() {
  return (
